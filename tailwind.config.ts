aae49a09e0770db0a943dd10f6824f99590b79b0
kadam raval <ravalkadam@gmail.com>
2025-07-12 19:52:40 +0000

    no we need more to reduce for large scree only not reduce md screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,10 @@ export default {
         sm: '2rem',
       },
       screens: {
-        '2xl': '1200px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1200px',
       },
     },
     extend: {
