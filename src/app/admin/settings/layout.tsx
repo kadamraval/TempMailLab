37d23b57b174a5db1591fa4a1b96b82111612c64
kadam raval <ravalkadam@gmail.com>
2025-07-15 20:42:25 +0000

    Setting Sidbar width is long compact it

--- src/app/admin/settings/layout.tsx
+++ src/app/admin/settings/layout.tsx
@@ -6,7 +6,7 @@ interface SettingsLayoutProps {
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
-    <div className="grid md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr] gap-6">
+    <div className="grid md:grid-cols-[180px_1fr] lg:grid-cols-[200px_1fr] gap-6">
       <SettingsSidebar />
       <div>
         {children}
