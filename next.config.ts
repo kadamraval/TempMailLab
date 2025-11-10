5b7bbbc7451fadfee90e1cab8c65d2ac5db49cce
kadam raval <ravalkadam@gmail.com>
2025-11-10 21:39:38 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
+        protocol: 'https' as const,
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
         protocol: 'https',
         hostname: 'i.pravatar.cc',
         port: '',
