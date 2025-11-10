40f2ec66c565d76469d03dab6f08b7529f06dff2
kadam raval <ravalkadam@gmail.com>
2025-11-10 21:42:56 +0000

    not movieng

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,10 +102,16 @@ export default {
             height: '0',
           },
         },
+        "border-beam-animate": {
+          "100%": {
+            "offset-distance": "100%",
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        "border-beam": "border-beam-animate var(--duration) linear infinite",
       },
     },
   },
