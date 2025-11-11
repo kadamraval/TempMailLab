fb186669ab715a3fa17455f2927093c72e65e827
kadam raval <ravalkadam@gmail.com>
2025-11-11 07:51:34 +0000

    Right side getting space when scroll, why scroll seeing in bottom

--- src/components/dot-background.tsx
+++ src/components/dot-background.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { cn } from "@/lib/utils";
 
 export function DotBackground({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex flex-col items-center justify-center">
+    <div className={cn("w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex flex-col items-center justify-center overflow-hidden")}>
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="relative z-20 w-full">
