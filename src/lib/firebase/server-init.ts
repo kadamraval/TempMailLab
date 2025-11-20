76a02eae7bdcad1e9d3a94d3281e2874e1c7800a
kadam raval <ravalkadam@gmail.com>
2025-11-20 00:11:40 +0000

    If server start it will show right

--- src/lib/firebase/server-init.ts
+++ src/lib/firebase/server-init.ts
@@ -1,5 +1,5 @@
 
-import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
+import { initializeApp, getApps, App } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
