86e5f72723e2ce592491cef09e0280052ac9fe41
kadam raval <ravalkadam@gmail.com>
2025-11-20 12:58:15 +0000

    ?

--- src/lib/firebase/server-init.ts
+++ src/lib/firebase/server-init.ts
@@ -1,4 +1,3 @@
-
 import { initializeApp, getApps, App, cert, ServiceAccount } from 'firebase-admin/app';
 import { getFirestore, Firestore } from 'firebase-admin/firestore';
 import { getAuth, Auth } from 'firebase-admin/auth';
