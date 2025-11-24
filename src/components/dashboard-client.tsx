a6727fca299924dcae13f8f7b2538a0abb5867e2
kadam raval <ravalkadam@gmail.com>
2025-11-24 00:49:46 +0000

    Try fixing this error: `Runtime Error: FirebaseError: The query requires

--- src/components/dashboard-client.tsx
+++ src/components/dashboard-client.tsx
@@ -182,7 +182,6 @@ export function DashboardClient() {
             const userInboxesQuery = query(
                 collection(firestore, 'inboxes'), 
                 where('userId', '==', activeUser.uid),
-                orderBy('createdAt', 'desc'),
                 limit(1)
             );
             const userInboxesSnap = await getDocs(userInboxesQuery);
