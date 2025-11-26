22caac1a5c03986c82b33cd5b19146ac435be2b4
kadam raval <ravalkadam@gmail.com>
2025-11-26 20:23:23 +0000

    Now finish blog and categories task, wordpress like

--- src/app/(admin)/admin/blog/page.tsx
+++ src/app/(admin)/admin/blog/page.tsx
@@ -1,18 +1,13 @@
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+'use client';
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function AdminBlogPage() {
   return (
-    <div className="container mx-auto py-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>Manage Blog Posts</CardTitle>
-          <CardDescription>Create, edit, and publish blog posts.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Blog post management interface will be displayed here.</p>
-        </CardContent>
-      </Card>
-    </div>
+    <Card>
+      <CardContent className="p-6">
+        <p>Blog post management coming soon...</p>
+      </CardContent>
+    </Card>
   );
 }
--- src/app/(admin)/admin/categories/page.tsx
+++ src/app/(admin)/admin/categories/page.tsx
@@ -1,18 +1,13 @@
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+'use client';
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function AdminCategoriesPage() {
   return (
-    <div className="container mx-auto py-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>Manage Categories</CardTitle>
-          <CardDescription>Organize blog posts into categories.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Category management interface will be displayed here.</p>
-        </CardContent>
-      </Card>
-    </div>
+     <Card>
+      <CardContent className="p-6">
+        <p>Category management coming soon...</p>
+      </CardContent>
+    </Card>
   );
 }
