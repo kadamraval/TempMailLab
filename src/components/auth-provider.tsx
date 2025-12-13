d7a994c4f7717e952fbee29b44e907d95917515f
kadam raval <ravalkadam@gmail.com>
2025-12-13 20:38:52 +0000

    your joking with code not seeing your involment do you still not solve p

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React, { createContext, useEffect, useState, ReactNode, useContext } from 'react';
--- src/firebase/auth/use-user.tsx
+++ src/firebase/auth/use-user.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useFirebase } from '../provider';
